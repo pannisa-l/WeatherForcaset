f59ee4eb0c8cbb188536971d304c0dd008ab41ae
pannisal <pannisa.lekphet94@gmail.com>
2022-03-17 11:53:00 +0700

    update font

---
+++ WeatherForecast/CurrentWeather/CurrentViewController.swift
@@ -0,0 +1,195 @@
+//
+//  ViewController.swift
+//  WeatherForcaset
+//
+//  Created by nisa.eem on 3/14/22.
+//
+
+import UIKit
+import Kingfisher
+
+class CurrentViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet var viewScreen: UIView!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var tempData: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var humidityData: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet weak var imageWeather: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var weatherStatus: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var rightPageButton: UIButton!
+    @IBOutlet weak var descripWeatherLabel: UILabel!
+    @IBOutlet weak var changTampButton: UIButton!
+    
+    private var viewmodel = LoadWeatherViewModel()
+    
+    var weatherItem = WeatherItem()
+    var fivedayForecast = FivedayForecast()
+    var city : String = "Bangkok"
+    var temp : Int = 0
+    var dateCurrent : String = ""
+    var idIcon : String = ""
+    var imgIcon: UIImage?
+    var celsius = true
+    let dateFormatter = DateFormatter()
+    let date = Date()
+   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.isHidden = true
+        hideKeyboardWhenTappedAround()
+        searchTextField.delegate = self
+        searchTextField.clearButtonMode = .whileEditing
+        self.getCurrentWeather(self.city)
+        self.setView()
+    }
+    
+    func setView(){
+        //set font and color
+        self.titleLabel.text = "Weather"
+        self.titleLabel.font = UIFont(name: "Montserrat Black", size: 45)
+      
+        self.rightPageButton.setImage(UIImage(systemName: "arrow.right"), for: .normal)
+        self.rightPageButton.tintColor = UIColor(named: "buttonTemp")
+        self.rightPageButton.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+
+        self.changTampButton.setTitle("°C", for: .normal)
+        self.changTampButton.backgroundColor = UIColor(named: "buttonTemp")
+        self.changTampButton.setTitleColor(UIColor.white, for: .normal)
+        self.changTampButton.layer.cornerRadius = self.changTampButton.frame.size.height/2
+        self.changTampButton.clipsToBounds = true
+
+        self.searchTextField.placeholder = "Ex. London"
+        self.searchTextField.font = UIFont(name: "Montserrat Light", size: 18)
+        
+        self.searchButton.setTitle("Search", for: .normal)
+        self.searchButton.setTitleColor(UIColor.white, for: .normal)
+        self.searchButton.backgroundColor = UIColor(named: "buttonTemp")
+        self.searchButton.layer.cornerRadius = self.searchButton.frame.size.height/2
+        self.searchButton.clipsToBounds = true
+
+
+        self.cityLabel.font = UIFont(name: "Montserrat Bold", size: 30)
+        self.dateLabel.font = UIFont(name: "Montserrat Light", size: 20)
+        
+// 
+//        self.imageWeather.layer.borderColor = UIColor(named: "BorderImg")?.cgColor
+//        self.imageWeather.layer.borderWidth = 3
+//        self.imageWeather.layer.cornerRadius = self.imageWeather.frame.height / 2
+//        self.imageWeather.clipsToBounds = true
+        
+        self.tempLabel.text = "Temperature"
+        self.humidityLabel.text = "Humidity"
+        self.tempLabel.font = UIFont(name: "Montserrat Medium", size: 20)
+        self.humidityLabel.font = UIFont(name: "Montserrat Medium", size: 20)
+        self.dateLabel.font = UIFont(name: "Montserrat Light", size: 20)
+        self.weatherStatus.font = UIFont(name: "Montserrat Bold", size: 22)
+        self.descripWeatherLabel.font = UIFont(name: "Montserrat Light", size: 18)
+        self.tempData.font = UIFont(name: "Montserrat Medium", size: 25)
+        self.humidityData.font = UIFont(name: "Montserrat Medium", size: 25)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        setGradientBackground()
+        super.viewWillAppear(animated)
+    }
+    
+    func setGradientBackground() {
+        let colorTop =  UIColor.white.cgColor
+        let colorBottom = UIColor.blue.cgColor
+                    
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [colorTop, colorBottom]
+        gradientLayer.locations = [0.0, 3.0]
+        gradientLayer.frame = self.view.bounds
+                
+        self.view.layer.insertSublayer(gradientLayer, at:0)
+    }
+    
+    func getCurrentWeather(_ city: String){
+        viewmodel.loadCurrentWeather(city, completeHandle: { (data) in
+            self.weatherItem = data
+            
+            if self.weatherItem.cod == 200 {
+                self.idIcon = self.weatherItem.weather?[0].icon ?? ""
+                self.getIcon(self.idIcon)
+                
+                self.city = "\(self.weatherItem.name ?? "")"
+                self.cityLabel.text = self.city
+                self.descripWeatherLabel.text = "\(self.weatherItem.weather?[0].description ?? "")"
+                
+                self.dateFormatter.locale = Locale(identifier: "en")
+                self.dateFormatter.dateFormat = "EEEE d MMMM yyyy"
+                self.dateCurrent = self.dateFormatter.string(from: self.date)
+                self.dateLabel.text = self.dateCurrent
+                
+                self.weatherStatus.text = "\(self.weatherItem.weather?[0].main ?? "")"
+                self.temp = Int((self.weatherItem.main?.temp ?? 0.0) - 273.15)
+                self.tempData.text = "\(self.temp)°C"
+                self.humidityData.text = "\(self.weatherItem.main?.humidity ?? 0)%"
+            } else {
+                self.showToast(message: "ไม่พบข้อมูล", seconds: 3.0)
+            }
+        })
+    }
+    
+    func getIcon(_ id:String){
+        let url = URL(string: "https://openweathermap.org/img/wn/\(id)@4x.png")
+        self.imageWeather.kf.setImage(with: url)
+    }
+    
+    @IBAction func changTemp(_ sender: Any) {
+        if self.celsius == true {
+        self.celsius = false
+        let tempChangButton = Int((self.temp*9)/5 + 32)
+        self.tempData.text = "\(tempChangButton)°F"
+            self.temp = tempChangButton
+            self.changTampButton.setTitle("°F", for: .normal)
+        }else{
+            self.celsius = true
+            let tempChangButton = Int((self.temp - 32)*5/9)
+            self.tempData.text = "\(tempChangButton)°C"
+            self.temp = tempChangButton
+            self.changTampButton.setTitle("°C", for: .normal)
+        }
+    }
+
+    
+    @IBAction func nextToFiveday(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "FivedayForecast", bundle: nil)
+        let destinationVC = storyboard.instantiateViewController(withIdentifier: "FivedayForecastViewController") as! FivedayForecastViewController
+        let fiveDayController = UINavigationController.init(rootViewController: destinationVC)
+        destinationVC.city = self.city
+        destinationVC.celsius = self.celsius
+        destinationVC.dateCurrent = self.dateCurrent
+        let transition = CATransition()
+        transition.type = CATransitionType.push
+        transition.subtype = CATransitionSubtype.fromRight
+        transition.timingFunction = CAMediaTimingFunction(name:CAMediaTimingFunctionName.easeInEaseOut)
+        self.viewScreen.window!.layer.add(transition, forKey: kCATransition)
+        fiveDayController.modalPresentationStyle = .fullScreen
+        self.present(fiveDayController, animated: false, completion: nil)
+    }
+    
+    @IBAction func searchButtonAction(_ sender: Any) {
+        self.city = self.searchTextField.text ?? ""
+        getCurrentWeather(self.city)
+        self.searchTextField.resignFirstResponder()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.city = textField.text ?? ""
+        getCurrentWeather(self.city)
+        print("\(self.city)")
+        textField.resignFirstResponder()
+        return true
+    }
+ 
+}
+
---
+++ WeatherForecast/ViewModel/LoadWeatherViewModel.swift
@@ -0,0 +1,205 @@
+//
+//  LoadWeather.swift
+//  WeatherForcaset
+//
+//  Created by nisa.eem on 3/14/22.
+//
+
+import Foundation
+import SwiftyJSON
+
+class LoadWeatherViewModel : NSObject {
+    
+    let url : String = "https://api.openweathermap.org/data/2.5/"
+    let apiKey : String = "631556fef5fc60966e29819bdd4a0bdf"
+    var weatherItem = WeatherItem()
+    var errorLoad = ErrorLoad()
+    var fivedayForecast = [FivedayForecast]()
+    var data : String = ""
+    let dateFormatter = DateFormatter()
+    let date = Date()
+    
+    func loadCurrentWeather(_ city: String,completeHandle: @escaping((_ weather: WeatherItem)->())){
+        
+        let newString = city.replacingOccurrences(of: " ", with: "%20", options: .literal, range: nil)
+        print("string \(newString)")
+        
+        let urlString = "\(self.url)weather?q=\(newString)&appid=\(self.apiKey)"
+        
+        Network.requestWithParameter(url: urlString, method: .get, params: nil) { (response, error) in
+            if let error = error {
+                print(error)
+                return
+            }
+            else {
+                if let response = response {
+                    let responseString = String(data: response as! Data, encoding: .utf8)
+                    self.data = responseString ?? ""
+                    if self.data.contains("200") {
+                        let decode = try! JSONDecoder().decode(WeatherItem.self, from:  self.data.data(using: .utf8)!)
+                        
+                        self.weatherItem = decode
+                        print(self.weatherItem)
+                        completeHandle(self.weatherItem)
+                    } else {
+                        let decode = try! JSONDecoder().decode(ErrorLoad.self, from:  self.data.data(using: .utf8)!)
+                        self.errorLoad = decode
+                        print(self.errorLoad)
+                        completeHandle(WeatherItem())
+                    }
+                }
+            }
+        }
+    }
+    
+    func loadFivedayforecast(_ city: String,completeHandle: @escaping((_ weather: [FivedayForecast])->())) {
+        
+        let newString = city.replacingOccurrences(of: " ", with: "%20", options: .literal, range: nil)
+        print("string \(newString)")
+        
+        let urlString = "\(self.url)forecast?q=\(newString)&appid=\(self.apiKey)"
+        self.dateFormatter.locale = Locale(identifier: "en")
+        self.dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        
+        Network.requestWithParameter(url: urlString, method: .get, params: nil) { (response, error) in
+            if let error = error {
+                print(error)
+                return
+            }
+            else {
+                if let response = response {
+                    let responseString = String(data: response as! Data, encoding: .utf8)
+                    self.data = responseString ?? ""
+                    if self.data.contains("200") {
+                        let forecastDecode = try! JSONDecoder().decode(WeatherForecast.self, from:  self.data.data(using: .utf8)!)
+                        print(forecastDecode)
+                        self.setDatainArray(forecastDecode, completeHandle: { data in
+                            self.fivedayForecast = data
+                        })
+                        completeHandle(self.fivedayForecast)
+                    }
+                } else {
+                    let decode = try! JSONDecoder().decode(ErrorLoad.self, from:  self.data.data(using: .utf8)!)
+                    self.errorLoad = decode
+                    print(self.errorLoad)
+                    completeHandle([FivedayForecast]())
+                }
+            }
+        }
+    }
+    
+    
+    func setDatainArray(_ forecastDecode: WeatherForecast,completeHandle: @escaping((_ fivedayForecast: [FivedayForecast])->())){
+        
+        var totalData = forecastDecode.list.count
+        let loop = forecastDecode.list.count - 1
+        print("loop = \(totalData)")
+        
+        var forecastmodelArray : [FivedayForecast] = []
+        
+        //day
+        var currentDay = FivedayForecast(day: "", weatherData: [])
+        var secondDay = FivedayForecast(day: "", weatherData: [])
+        var thirdDay = FivedayForecast(day: "", weatherData: [])
+        var fourthDay = FivedayForecast(day: "", weatherData: [])
+        var fifthDay = FivedayForecast(day: "", weatherData: [])
+        
+        //data in day
+        var weatherData : [WeatherData] = []
+        var currentForecast : [WeatherData] = []
+        var secondForecast : [WeatherData] = []
+        var thirdForecast : [WeatherData] = []
+        var fourthForecast : [WeatherData] = []
+        var fifthForecast : [WeatherData] = []
+        var sixthForecast : [WeatherData] = []
+        
+        for day in 0...loop {
+            
+            let temp = forecastDecode.list[day].main?.temp
+            let descrip = forecastDecode.list[day].weather?[0].description
+            let icon = forecastDecode.list[day].weather?[0].icon
+            let dt_txt = forecastDecode.list[day].dt_txt
+            
+            let date = self.dateFormatter.date(from: forecastDecode.list[day].dt_txt ?? "")
+            let calendar = Calendar.current
+            let components = calendar.dateComponents([.weekday], from: date!)
+            let weekdaycomponent = components.weekday! - 1
+            
+            let weekday = self.dateFormatter.weekdaySymbols[weekdaycomponent]
+            let currentDayComponent = calendar.dateComponents([.weekday], from: Date())
+            let currentWeekDay = currentDayComponent.weekday! - 1
+            let currentweekdaysymbol = self.dateFormatter.weekdaySymbols[currentWeekDay]
+            
+            if weekdaycomponent == currentWeekDay - 1 {
+                totalData = totalData - 1
+            }
+            
+            //add data in forecastDecode list
+            if weekdaycomponent == currentWeekDay {
+                let info = WeatherData(temp: temp, description: descrip, icon: icon, date: dt_txt)
+                currentForecast.append(info)
+                currentDay = FivedayForecast(day: currentweekdaysymbol, weatherData: currentForecast)
+                weatherData.append(info)
+            }
+            if weekdaycomponent == currentWeekDay.increment(index: 1) {
+                let info = WeatherData(temp: temp, description: descrip, icon: icon, date: dt_txt)
+                secondForecast.append(info)
+                secondDay = FivedayForecast(day: weekday, weatherData: secondForecast)
+                weatherData.append(info)
+            }
+            if weekdaycomponent == currentWeekDay.increment(index: 2) {
+                let info = WeatherData(temp: temp, description: descrip, icon: icon, date: dt_txt)
+                thirdForecast.append(info)
+                thirdDay = FivedayForecast(day: weekday, weatherData: thirdForecast)
+                weatherData.append(info)
+            }
+            if weekdaycomponent == currentWeekDay.increment(index: 3) {
+                let info = WeatherData(temp: temp, description: descrip, icon: icon, date: dt_txt)
+                fourthForecast.append(info)
+                fourthDay = FivedayForecast(day: weekday, weatherData: fourthForecast)
+                weatherData.append(info)
+            }
+            if weekdaycomponent == currentWeekDay.increment(index: 4){
+                let info = WeatherData(temp: temp, description: descrip, icon: icon, date: dt_txt)
+                fifthForecast.append(info)
+                fifthDay = FivedayForecast(day: weekday, weatherData: fifthForecast)
+                weatherData.append(info)
+            }
+            if weekdaycomponent == currentWeekDay.increment(index: 5) {
+                let info = WeatherData(temp: temp, description: descrip, icon: icon, date: dt_txt)
+                sixthForecast.append(info)
+                weatherData.append(info)
+            }
+            
+            //check data in array = tatal data list
+            if weatherData.count == totalData {
+                //1 Day current
+                if currentDay.weatherData.count > 0 {
+                    forecastmodelArray.append(currentDay)
+                }
+                //2 Day
+                if secondDay.weatherData.count > 0 {
+                    forecastmodelArray.append(secondDay)
+                }
+                //3 Day
+                if thirdDay.weatherData.count > 0 {
+                    forecastmodelArray.append(thirdDay)
+                }
+                //4 Day
+                if fourthDay.weatherData.count > 0 {
+                    forecastmodelArray.append(fourthDay)
+                }
+                //5 Day
+                if fifthDay.weatherData.count > 0 {
+                    forecastmodelArray.append(fifthDay)
+                }
+                if forecastmodelArray.count <= 5 {
+                    print(forecastmodelArray)
+                    print(forecastmodelArray.count)
+                    completeHandle(forecastmodelArray)
+                }
+            }
+        }
+        
+    }
+}
